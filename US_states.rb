5152f4199646062e06e01b18914231a8ed1afe7a
Satwass <57160438@go.buu.ac.th>
2016-09-17 15:00:33 +0700

    summit Quiz 57160438 Satawass Srisan

---
+++ US_states.rb
@@ -0,0 +1,75 @@
+﻿US_states = 
+
+	{"Alabama" => "AL",
+	"Alaska" => "AK",
+	"Arizona" => "AZ",
+	"Arkansas" => "AR",
+	"California" => "CA",
+	"Colorado" => "CO",
+	"Connecticut" => "CT",
+	"Delaware" => "DE",
+	"District of Columbia" => "DC",
+	"Florida" => "FL",
+	"Georgia" => "GA",
+	"Hawaii" => "HI",
+	"Idaho" => "ID",
+	"Illinois" => "IL",
+	"Indiana" => "IN",
+	"Iowa" => "IA",
+	"Kansas" => "KS",
+	"Kentucky" => "KY",
+	"Louisiana" => "LA",
+	"Maine" => "ME",
+	"Maryland" => "MD",
+	"Massachusetts" => "MA",
+	"Michigan" => "MI",
+	"Minnesota" => "MN",
+	"Mississippi" => "MS",
+	"Missouri" => "MO",
+	"Montana" => "MT",
+	"Nebraska" => "NE",
+	"Nevada" => "NV",
+	"New Hampshire" => "NH",
+	"New Jersey" => "NJ",
+	"New Mexico" => "NM",
+	"New York" => "NY",
+	"North Carolina" => "NC",
+	"North Dakota" => "ND",
+	"Ohio" => "OH",
+	"Oklahoma" => "OK",
+	"Oregon" => "OR",
+	"Pennsylvania" => "PA",
+	"Rhode Island" => "RI",
+	"South Carolina" => "SC",
+	"South Dakota" => "SD",
+	"Tennessee" => "TN",
+	"Texas" => "TX",
+	"Utah" => "UT",
+	"Vermont" => "VT",
+	"Virginia" => "VA",
+	"Washington" => "WA",
+	"West Virginia" => "WV",
+	"Wisconsin" => "WI",
+	"Wyoming" => "WY"}
+	
+US_NT = {}	
+US_states.each {
+    |name, stand|
+    if(stand[1] == "T" || stand[1] == "N")
+        US_NT[name] = stand
+    end
+    
+}
+
+US_NT_sort = US_NT.sort {|item1, item2| item2 <=> item1}
+US_final = {}
+
+US_NT_sort.each {
+    |name, stand|
+    if (name[0] == "A" || name[0] == "E" || name[0] == "I" || name[0] == "O" || name[0] == "U") && (name[name.length-1] == "a" || name[name.length-1] == "e" || name[name.length-1] == "i" || name[name.length-1] == "o" || name[name.length-1] == "u")
+        US_final[name] = stand
+        puts name
+    end
+}
+
+
---
+++ prime_hash.rb
@@ -0,0 +1,33 @@
+﻿def prime(n)
+  puts "That's not an integer." unless n.is_a? Integer
+  is_prime = true
+  for i in 2..n-1
+    if n % i == 0
+      is_prime = false
+    end
+  end
+  return is_prime
+end
+
+def prime_hash(number)
+    pk = {}
+    count = 1
+    i = 2
+    loop do 
+        break if count > number
+        if prime(i)
+            pk["p"+count.to_s] = i
+            count +=1
+        end
+        i += 1
+    end
+    
+    return pk
+end
+
+sum = prime_hash(5)
+
+sum.each {
+    |key, value|
+    puts "#{key} => #{value}"
+}
